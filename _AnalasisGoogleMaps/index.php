a5a931489ceb2d8758f572cd8fb6d813f73f4512
Nicolas <nw@netstarter.dk>
2012-12-02 00:06:20 +0100

    GMAPS update (Lines drawn pr. positions array)
    GMAPS update (Lines drawn pr. positions array)

--- _AnalasisGoogleMaps/index.php
+++ _AnalasisGoogleMaps/index.php
@@ -18,6 +18,7 @@ var json = <?php echo $message; ?>;
 // Make new array for GMAPS
 gmapscoordinates = new Array (10000);
 var map;
+var map2;
 
 // Iterate json array
 for(var i = 0; i < json.length; i++)
@@ -46,7 +47,6 @@ for(var i = 0; i < json.length; i++)
         }
     }
 }
-alert(gmapscoordinates[0][0][1]);
 
 
 map = new GMaps({
@@ -58,7 +58,7 @@ map = new GMaps({
     }
   });
   
-path = [[-12.044012922866312, -77.02470665341184], [-12.05449279282314, -77.03024273281858], [-12.055122327623378, -77.03039293652341], [-12.075917129727586, -77.02764635449216], [-12.07635776902266, -77.02792530422971], [-12.076819390363665, -77.02893381481931], [-12.088527520066453, -77.0241058385925], [-12.090814532191756, -77.02271108990476]];
+//map 1
 
   map.drawPolyline({
     path: gmapscoordinates[0],
@@ -67,12 +67,22 @@ path = [[-12.044012922866312, -77.02470665341184], [-12.05449279282314, -77.0302
     strokeWeight: 6
   });
   
+//map 2
+  
+  map.drawPolyline({
+	    path: gmapscoordinates[1],
+	    strokeColor: '#ff0000',
+	    strokeOpacity: 0.6,
+	    strokeWeight: 6
+	  });
+
 });
 
 
 </script>
 
 <center><div id="map"></div></center>
+
 
 </body>
 </html>
